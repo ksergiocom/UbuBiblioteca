aa62f775b8883d84494b3ed605490f2da92a10e6
sergio <sergio@ksergio.com>
2024-10-02 20:58:49 +0200

    Componentes
    ¿Como funcionan los compoenntes  de svelte?

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let label = "Label"
+	export let placeholder
+</script>
+
+<div class="mb-3">
+	<label for="exampleFormControlInput1" class="form-label"
+		>{label}</label
+	>
+	<input
+		type="email"
+		class="form-control"
+		id="exampleFormControlInput1"
+		{placeholder}
+	/>
+</div>
--- src/routes/catalogo/+page.svelte
+++ src/routes/catalogo/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+	import Input from '$lib/components/Input.svelte'
+	import Button from '$lib/components/Button.svelte'
+</script>
 
-
-<h1>Catalogo</h1>
+<h1 class="pb-3">Catalogo</h1>
+<Input label="Nombre"/>
+<Button>Guardar</Button>
